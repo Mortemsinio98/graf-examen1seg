a95e137dbf905eb5035ae5643e9c36e276a6709a
Gildardo_Vazquez <gildardo.jose.vazquez@gmail.com>
2026-02-18 09:54:03 -0600

    Ajustes SVG + Control de dimensiónn

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-// Obtener la imagen y los botones
+// Obtener elementos
 const image = document.getElementById('image');
+
+const xSmallBtn = document.getElementById('xSmallBtn');
 const smallBtn = document.getElementById('smallBtn');
 const mediumBtn = document.getElementById('mediumBtn');
 const largeBtn = document.getElementById('largeBtn');
+const xLargeBtn = document.getElementById('xLargeBtn');
 
-// Funciones para cambiar el tamaño de la imagen
+// Eventos
+
+xSmallBtn.addEventListener('click', () => {
+    image.style.width = '100px';
+    image.style.height = 'auto';
+});
+
 smallBtn.addEventListener('click', () => {
-    image.style.width = '150px';
-    image.style.height = '150px';
+    image.style.width = '200px';
+    image.style.height = 'auto';
 });
 
 mediumBtn.addEventListener('click', () => {
     image.style.width = '300px';
-    image.style.height = '300px';
+    image.style.height = 'auto';
 });
 
 largeBtn.addEventListener('click', () => {
-    image.style.width = '450px';
-    image.style.height = '450px';
+    image.style.width = '400px';
+    image.style.height = 'auto';
+});
+
+xLargeBtn.addEventListener('click', () => {
+    image.style.width = '550px';
+    image.style.height = 'auto';
 });
